755d1fd69f97462b788b098daa73b5b9d7e04d47
“Yan5_xin_ing” <3163835621@qq.com>
2024-10-19 13:22:10 +0800

    更新软件版本(1.5.0)

--- applications/version.h
+++ applications/version.h
@@ -16,35 +16,27 @@
 extern "C" {
 #endif
 
-#include "app.h"
+#include "app_ofsm.h"
 
 /*
  * V8-Software version information
  */
 #define SOFTWARE_VERSION       1L
-#define SOFTWARE_SUBVERSION    4L
-#define SOFTWARE_REVISION      7L   /*  ([1.2.5 (修复四组模块的柔性切换模式下，一组继电器被误断问题)]
+#define SOFTWARE_SUBVERSION    5L
+#define SOFTWARE_REVISION      0L   /*  ([1.2.5 (修复四组模块的柔性切换模式下，一组继电器被误断问题)]
                                          [1.3.7 为能佳新板子将能佳默认IP、端口、二维码前缀写入flash]
                                          [1.3.8为能佳修改并充时CCS输出电流值]
                                          [1.4.1 为模块离线或故障也不踢出，去掉CCS与BCS电流比较，保留CCS与BCL电流比较]
                                          [1.4.2 为模块离线或故障也不踢出，去掉CCS与BCS电流比较，去掉CCS与BCL电流比较]
-                                         [1.4.3 为模块离线或故障则踢出，CCS电流与模块电流比较，模块电流与BCL电流比较，仅用于测试]) */
+                                         [1.4.3 为模块离线或故障则踢出，CCS电流与模块电流比较，模块电流与BCL电流比较，仅用于测试]
+                                         [1.4.8 为打印开log查灯全亮和费率不显示问题，最后提交日期：20241016 16:03]
+                                         [1.4.9 为打印开log费率不显示问题(增加打印信息)) */
 
-#ifdef HMI_T5UIC1_ENABLE
-#define SOFTWARE_HMIION       'B'
+#ifdef APP_USING_DOUBLEGUN
+#define SOFTWARE_MODULE                 "YKC7103"          /* 软件型号 */
 #else
-#define SOFTWARE_HMIION       'A'
-#endif
-
-#define SOFTWARE_RCVERSION     0L
-
-/*
- * V8-Hardware version information
- */
-#define HARDWARE_VERSION       '8'
-#define HARDWARE_SUBVERSION    '8'
-#define HARDWARE_REVISION1     '2'
-#define HARDWARE_REVISION2     '7'
+#define SOFTWARE_MODULE                 "YKC7101"          /* 软件型号 */
+#endif /* APP_USING_DOUBLEGUN */
 
 #ifdef __cplusplus
 }
