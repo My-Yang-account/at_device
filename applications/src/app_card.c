6074f78e50a4eb4030de593e73ef88b1895ef0d4
My-Yang-account <3163835621@qq.com>
2025-05-06 16:57:12 +0800

    fix: 离线计费模式时不使能离线卡模式时不读卡信息(防止卡被锁)

--- applications/src/app_card.c
+++ applications/src/app_card.c
@@ -866,6 +866,9 @@ static int32_t app_card_info_process(void* handle)
         }
         return 0x00;
     }
+    if((*(sys_read_config_item_content(CONFIG_ITEM_SUPORT_OFFLINE_CARD, 0))) == APP_THA_ENUM_FALSE){
+        return APP_CARD_OPERATE_RET_NULL;
+    }
     s_rfidr = (rfid_reader*)handle;
 
     int32_t ret = 0x00;
