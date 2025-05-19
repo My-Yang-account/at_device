12fc96cd29648d13c9324a0ff33968a3bb3cf5a0
My-Yang-account <3163835621@qq.com>
2025-05-19 22:27:06 +0800

    fix: 增大读卡器线程栈

--- rfid_reader/inc/app_rfid_reader.h
+++ rfid_reader/inc/app_rfid_reader.h
@@ -19,7 +19,7 @@
 #define RFIDR_NODE_RUNNING_OPTION_NAME                  (1 <<0x03)                     /** 节点运行选项字：线程名字 */
 
 #define RFIDR_MAIL_NUM_MAX                               0x05                          /** 射频读卡器邮箱邮件数量 */
-#define RFIDR_THREAD_STACK_SIZE                          2048                          /** 射频读卡器线程栈大小(B) */
+#define RFIDR_THREAD_STACK_SIZE                          3072                          /** 射频读卡器线程栈大小(B) */
 
 #define RFIDR_UUID_LEN_MAX                               0x08                          /** 卡UUID长度 */
 #define RFIDR_CARD_NUMBER_LEN_MAX                        0x10                          /** 卡号长度 */
