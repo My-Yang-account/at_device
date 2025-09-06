2f7e0db0c7ac9385db5cb559527eeb328e0877ce
My-Yang-account <3163835621@qq.com>
2025-09-06 13:31:56 +0800

    更新软件版本(单枪)[1.7.8_01(1.7.I_01)]

--- applications/version.h
+++ applications/version.h
@@ -96,7 +96,7 @@ extern "C" {
 
 #define SOFTWARE_VERSION       1L
 #define SOFTWARE_SUBVERSION    7L
-#define SOFTWARE_REVISION      6L  /* 01 */
+#define SOFTWARE_REVISION      8L  /* 01 */
 
 #endif /* APP_USING_DOUBLEGUN */
 
