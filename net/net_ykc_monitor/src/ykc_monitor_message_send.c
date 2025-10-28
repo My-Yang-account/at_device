6cf5d096a150053037433afe107b1481ee47bd2c
My-Yang-account <3163835621@qq.com>
2025-10-28 10:48:47 +0800

    fix: 修改目标平台socket信息上报间隔为30分钟、目标平台计费信息上报间隔为30分钟

--- net/net_ykc_monitor/src/ykc_monitor_message_send.c
+++ net/net_ykc_monitor/src/ykc_monitor_message_send.c
@@ -21,8 +21,8 @@
 #ifdef NET_PACK_USING_YKC_MONITOR
 
 #ifdef NET_YKC_MONITOR_USING_EXTEND_PROTOCOL
-#define NET_YKC_MONITOR_TPLAT_SOCKET_INFO_INTERVAL                   (5 *60000)  /* 目标平台socket信息上报间隔 */
-#define NET_YKC_MONITOR_BILLING_RULE_INFO_INTERVAL                   (5 *60000)  /* 目标平台计费信息上报间隔 */
+#define NET_YKC_MONITOR_TPLAT_SOCKET_INFO_INTERVAL                   (30 *60000)  /* 目标平台socket信息上报间隔 */
+#define NET_YKC_MONITOR_BILLING_RULE_INFO_INTERVAL                   (30 *60000)  /* 目标平台计费信息上报间隔 */
 #define NET_YKC_MONITOR_TIMESYNC_INTERVAL                            (8 *3600 *1000)  /* 向平台请求对时间隔 */
 #endif /* #ifdef NET_YKC_MONITOR_USING_EXTEND_PROTOCOL */
 
