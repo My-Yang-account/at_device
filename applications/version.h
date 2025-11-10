b0d4d48feae0021efaceb23c3d5858420a0b7537
My-Yang-account <3163835621@qq.com>
2025-11-10 19:43:01 +0800

    更新软件版本(双枪：V2.1.D_01[V2.1.3_02])

--- applications/version.h
+++ applications/version.h
@@ -78,7 +78,7 @@ extern "C" {
 
 #define SOFTWARE_VERSION       2L
 #define SOFTWARE_SUBVERSION    1
-#define SOFTWARE_REVISION      3L  /** 02 */
+#define SOFTWARE_REVISION      3L  /* 02 */
 
 #else
 
