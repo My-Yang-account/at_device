1dbd6519f60ba58fc64f9dcb85b35def9dafbc1f
My-Yang-account <3163835621@qq.com>
2025-12-15 16:46:25 +0800

    更新软件版本(双枪：V2.1.J_01[V2.1.9_01])

--- applications/version.h
+++ applications/version.h
@@ -79,7 +79,7 @@ extern "C" {
 
 #define SOFTWARE_VERSION       2L
 #define SOFTWARE_SUBVERSION    1
-#define SOFTWARE_REVISION      8L  /* 02 */
+#define SOFTWARE_REVISION      9L  /* 02 */
 
 #else
 
