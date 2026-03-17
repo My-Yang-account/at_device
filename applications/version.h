8e07966c65b9dc4a5d633c6801652a0abd2834fe
My-Yang-account <3163835621@qq.com>
2026-03-17 13:58:02 +0800

    更新半矩/环矩软件版本[V1.0.B_02]

--- applications/version.h
+++ applications/version.h
@@ -94,7 +94,7 @@ extern "C" {
 #else
 #define SOFTWARE_VERSION       1L
 #define SOFTWARE_SUBVERSION    0
-#define SOFTWARE_REVISION      0L  /* 01 */
+#define SOFTWARE_REVISION      1L  /* 02 */
 #endif
 
 #else
